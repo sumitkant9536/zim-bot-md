2d84ebaee820c68dc86a5cba05fd21e56c8c4ccc
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-09 22:56:14 +0530

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -11,8 +11,5 @@ handler.help = ['ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold
 handler.tags = ['nsfw']
 handler.command = /^(ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
 
-handler.premium = false
-handler.register = true
-handler.nsfw = true
 
 module.exports = handler
